acd286130dcace3f32843b9253c4b9c383be497e
josepardo-eb <jose.pardo@eventbrite.com>
2024-02-04 18:36:27 +0100

    Initial commit

---
+++ theme.config.jsx
@@ -0,0 +1,16 @@
+export default {
+  footer: <p>Manchedev</p>,
+  head: ({ title, meta }) => (
+    <>
+      {meta.description && (
+        <meta name="description" content={meta.description} />
+      )}
+      {meta.tag && <meta name="keywords" content={meta.tag} />}
+      {meta.author && <meta name="author" content={meta.author} />}
+    </>
+  ),
+  readMore: "Read More →",
+  postFooter: null,
+  darkMode: false,
+  navs: [],
+};
