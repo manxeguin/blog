1015310efedea57f2749b8fa4a9001b50d380939
manxeguin <manxeguin1987@gmail.com>
2024-09-17 17:32:32 +0200

    add meta tags

--- theme.config.jsx
+++ theme.config.jsx
@@ -3,7 +3,15 @@ export default {
   head: ({ title, meta }) => (
     <>
       {meta.description && (
-        <meta name="description" content={meta.description} />
+        <>
+          <meta name="description" content={meta.description} />
+          <meta property="og:site_name" content="Manxeguin Dev" />
+          <meta property="og:description" content={meta.description}></meta>
+          <meta property="og:title" content={title} />
+          <meta name="twitter:site" content="Manxeguin Dev"></meta>
+          <meta name="twitter:title" content={title}/>
+          <meta name="twitter:description" content={meta.description}></meta>
+        </>
       )}
       {meta.tag && <meta name="keywords" content={meta.tag} />}
     </>
